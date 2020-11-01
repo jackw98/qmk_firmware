15645a727b4704c41478ebc4059d1c08c3ad7026
Ollie Spillett <ospillett@hotmail.com>
2020-11-01 16:23:20 +0000

    actually compiles now

--- keyboards/fission/keymaps/Ollie/keymap.c
+++ keyboards/fission/keymaps/Ollie/keymap.c
@@ -1,5 +1,9 @@
 #include QMK_KEYBOARD_H
 
+#define BASE 0
+#define ROW 1
+#define PAD 2
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base Layer
@@ -17,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                     KC_RCTRL,MO(PAD),   KC_ENTER,      KC_RSHIFT,KC_SPC, KC_BSPACE                  \
+                     KC_RCTRL,MO(PAD), KC_ENTER,      KC_RSHIFT,KC_SPC, KC_BSPACE                  \
 ),
 
 /* Layer 1
