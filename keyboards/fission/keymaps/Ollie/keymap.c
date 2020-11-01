11b0c9772e20f9b02aac4a2453428676d640da4a
Ollie Spillett <ospillett@hotmail.com>
2020-11-01 16:42:33 +0000

    added shorcuts layer

--- keyboards/fission/keymaps/Ollie/keymap.c
+++ keyboards/fission/keymaps/Ollie/keymap.c
@@ -3,6 +3,7 @@
 #define BASE 0
 #define ROW 1
 #define PAD 2
+#define SCUTS 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -21,10 +22,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                     KC_RCTRL,MO(PAD), KC_ENTER,      KC_RSHIFT,KC_SPC, KC_BSPACE                  \
+                     MO(PAD), MO(SCUTS),KC_ENTER,     KC_LSHIFT,KC_SPC, KC_BSPACE                  \
 ),
 
-/* Layer 1
+/* Row layout of number and F keys
  * ,----------------------------------.      ,-----------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |  F7  |  F8  |  F9  |  F10  |
  * |------+------+------+------+------|      |------+------+------+------+-------|
@@ -43,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,  KC_TAB,  KC_ESC                    \
 ),
 
-/* Layer 2 (numbers are number row keys so can type symbols using shift on right thumb from base layer)
+/* Pad layout of number and F keys (numbers are number row keys so can type symbols using shift on right thumb from base layer)
  * ,----------------------------------.      ,-----------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |      |      |      |   7  |   8  |   9  |   0   |
  * |------+------+------+------+------|      |------+------+------+------+-------|
@@ -60,6 +61,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,       KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, \
    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,       KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, \
                      KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TAB,  KC_ESC                    \
+),
+
+/* A layer to have a bunch of common shortcuts cos the keyboard doesn't have enough damn keys on it
+ * (a lot of the time acts like LCTRL with some additions)
+ * ,----------------------------------.      ,-----------------------------------.
+ * |AltTab|      |      |      |      |      |      |      |      |      |       |
+ * |------+------+------+------+------|      |------+------+------+------+-------|
+ * |      |      |      |      |      |      |      |      |      |      |       |
+ * |------+------+------+------+------|      |------+------+------+------+-------|
+ * |Ctrl+Z|Ctrl+X|Ctrl+C|Ctrl+V|      |      |      |      |      |      |       |
+ * '-------------+------+------+------|      |------+------+------+--------------'
+ *               |      |      |      |      |      |      |      | 
+ *               `--------------------'      `--------------------'
+ */
+
+[SCUTS] = LAYOUT(
+   LALT(KC_TAB),  KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+   KC_TRNS,       KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+   LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+                           KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS                   \
 )
 
 };
