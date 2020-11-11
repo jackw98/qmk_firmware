c4e3b7e5903dcaf615da33f72175c1891b748526
Ollie Spillett <ospillett@hotmail.com>
2020-11-11 14:13:43 +0000

    swapped position of esc key

--- keyboards/fission/keymaps/Ollie/keymap.c
+++ keyboards/fission/keymaps/Ollie/keymap.c
@@ -46,21 +46,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Pad layout of number and F keys (numbers are number row keys so can type symbols using shift on right thumb from base layer)
  * ,----------------------------------.      ,-----------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |   `  |      |      |   7  |   8  |   9  |   0   |
+ * |  F1  |  F2  |  F3  |  F4  |   `  |      |      |  7 & |  8 * |  9 ( |   0 ) |
  * |------+------+------+------+------|      |------+------+------+------+-------|
- * |  F5  |  F6  |  F7  |  F8  |      |      |      |   4  |   5  |   6  |       |
+ * |  F5  |  F6  |  F7  |  F8  |      |      |      |  4 $ |  5 % |  6 ^ |  ESC  |
  * |------+------+------+------+------|      |------+------+------+------+-------|
- * |  F9  |  F10 |  F11 |  F12 |      |      |      |   1  |   2  |   3  |  Ent  |
+ * |  F9  |  F10 |  F11 |  F12 |      |      |      |  1 ! |  2 @ |  3 # |  Ent  |
  * '-------------+------+------+------|      |------+------+------+--------------'
- *               |      |      |      |      |      |  Tab |  ESC | 
+ *               |      |      |      |      |      |  Tab |      | 
  *               `--------------------'      `--------------------'
  */
 
 [PAD] = LAYOUT(
    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_GRAVE,      KC_TRNS, KC_7,    KC_8,    KC_9,    KC_0,    \
-   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,       KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, \
+   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,       KC_TRNS, KC_4,    KC_5,    KC_6,    KC_ESC, \
    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,       KC_TRNS, KC_1,    KC_2,    KC_3,    KC_ENT, \
-                     KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TAB,  KC_ESC                    \
+                     KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TAB,  KC_TRNS                    \
 ),
 
 /* A layer to have a bunch of common shortcuts cos the keyboard doesn't have enough damn keys on it
