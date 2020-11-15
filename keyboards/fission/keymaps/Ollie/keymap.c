dc67d17b5a6031e0bb2d5a5482916797c1103e63
Ollie Spillett <ospillett@hotmail.com>
2020-11-15 21:15:52 +0000

    added ctrl+f functionality

--- keyboards/fission/keymaps/Ollie/keymap.c
+++ keyboards/fission/keymaps/Ollie/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "quantum.h"
 
 #define BASE 0
 #define GAME 1
@@ -42,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                     MO(PAD), KC_SPC,  KC_ENTER,      KC_LSHIFT,DF(BASE),KC_BSPACE                \
+                     MO(PAD), KC_SPC,  KC_ENTER,      KC_LSHIFT,TO(BASE),KC_BSPACE                \
 ),
 
 
@@ -80,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [PAD] = LAYOUT(
    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_GRAVE,      KC_TRNS, KC_7,    KC_8,    KC_9,    KC_0,    \
-   KC_F5,   KC_F6,   KC_F7,   KC_F8,   DF(GAME),       KC_TRNS, KC_4,    KC_5,    KC_6,    KC_ESC, \
+   KC_F5,   KC_F6,   KC_F7,   KC_F8,   TO(GAME),       KC_TRNS, KC_4,    KC_5,    KC_6,    KC_ESC, \
    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,       KC_TRNS, KC_1,    KC_2,    KC_3,    KC_ENT, \
                      KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TAB,  KC_TRNS                    \
 ),
@@ -89,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.      ,-----------------------------------.
  * | Alt  |      |Ctl+OS|      |Ctrl+T|      |  INS |      |  Up  |   [  |   ]   |
  * |------+------+------+------+------|      |------+------+------+------+-------|
- * |Ctrl+A|Ctrl+S|  OS  |      |      |      |  DEL | Left | Down | Right|   '   |
+ * |Ctrl+A|Ctrl+S|  OS  |Ctrl+F|      |      |  DEL | Left | Down | Right|   '   |
  * |------+------+------+------+------|      |------+------+------+------+-------|
  * |Ctrl+Z|Ctrl+X|Ctrl+C|Ctrl+V|      |      |      |  -   |  =   |   \  |   /   |
  * '-------------+------+------+------|      |------+------+------+--------------'
@@ -99,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [SCUTS] = LAYOUT(
    KC_LALT,    KC_TRNS, LCTL(KC_LWIN), KC_TRNS, LCTL(KC_T),       KC_INS,  KC_TRNS,       KC_UP,   KC_LBRC, KC_RBRC, \
-   LCTL(KC_A), LCTL(KC_S), KC_LWIN,    KC_TRNS,    KC_TRNS,       KC_DEL,  KC_LEFT,       KC_DOWN, KC_RIGHT,KC_QUOT, \
+   LCTL(KC_A), LCTL(KC_S), KC_LWIN,    LCTL(KC_F), KC_TRNS,       KC_DEL,  KC_LEFT,       KC_DOWN, KC_RIGHT,KC_QUOT, \
    LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS,       KC_TRNS, KC_MINS,       KC_EQL,  KC_BSLS, KC_SLSH, \
                            KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS, KC_TAB,        LSFT(KC_TAB)                   \
 )
