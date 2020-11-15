9655658d6809a1e9aaeb46b5d867283c35ed6f96
Ollie Spillett <ospillett@hotmail.com>
2020-11-15 20:36:45 +0000

    added gaming mode to swap spc to left side for fps not that i ever really play them

--- keyboards/fission/keymaps/Ollie/keymap.c
+++ keyboards/fission/keymaps/Ollie/keymap.c
@@ -1,9 +1,10 @@
 #include QMK_KEYBOARD_H
 
 #define BASE 0
-#define ROW 1
-#define PAD 2
-#define SCUTS 3
+#define GAME 1
+#define ROW 2
+#define PAD 3
+#define SCUTS 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -18,12 +19,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               |  PAD | SCUT |  Ent |      | RSHFT| Space| BKSP | 
  *               `--------------------'      `--------------------'
  */
-[0] = LAYOUT(
+[BASE] = LAYOUT(
    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
                      MO(PAD), MO(SCUTS),KC_ENTER,     KC_LSHIFT,KC_SPC, KC_BSPACE                  \
 ),
+
+
+/* Gaming base Layer
+ * ,----------------------------------.      ,-----------------------------------.
+ * |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P   |
+ * |------+------+------+------+------|      |------+------+------+------+-------|
+ * |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;   |
+ * |------+------+------+------+------|      |------+------+------+------+-------|
+ * |   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |   /   |
+ * '-------------+------+------+------|      |------+------+------+--------------'
+ *               |  PAD | Space|  Ent |      | RSHFT| BASE | BKSP | 
+ *               `--------------------'      `--------------------'
+ */
+[GAME] = LAYOUT(
+   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
+   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
+   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+                     MO(PAD), KC_SPC,  KC_ENTER,      KC_LSHIFT,DF(BASE),KC_BSPACE                \
+),
+
+
 
 /* Row layout of number and F keys (edit the layer moifier keys to have row rather than pad number layer)
  * ,----------------------------------.      ,-----------------------------------.
@@ -48,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.      ,-----------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |   `  |      |      |  7 & |  8 * |  9 ( |   0 ) |
  * |------+------+------+------+------|      |------+------+------+------+-------|
- * |  F5  |  F6  |  F7  |  F8  |      |      |      |  4 $ |  5 % |  6 ^ |  ESC  |
+ * |  F5  |  F6  |  F7  |  F8  | GAME |      |      |  4 $ |  5 % |  6 ^ |  ESC  |
  * |------+------+------+------+------|      |------+------+------+------+-------|
  * |  F9  |  F10 |  F11 |  F12 |      |      |      |  1 ! |  2 @ |  3 # |  Ent  |
  * '-------------+------+------+------|      |------+------+------+--------------'
@@ -58,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [PAD] = LAYOUT(
    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_GRAVE,      KC_TRNS, KC_7,    KC_8,    KC_9,    KC_0,    \
-   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,       KC_TRNS, KC_4,    KC_5,    KC_6,    KC_ESC, \
+   KC_F5,   KC_F6,   KC_F7,   KC_F8,   DF(GAME),       KC_TRNS, KC_4,    KC_5,    KC_6,    KC_ESC, \
    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,       KC_TRNS, KC_1,    KC_2,    KC_3,    KC_ENT, \
                      KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TAB,  KC_TRNS                    \
 ),
